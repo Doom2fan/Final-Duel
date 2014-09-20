4a56f2327f8786e1d03a5cc3ccf99a66bbb25715
Chronos Ouroboros <doom2.fan@gmail.com>
2014-09-20 12:47:37 -0300

    First commit of the Indentation tool. Unfinished (missing some features) and the code might be shitty.

---
+++ Tools/Auto-Indentation Tool/Auto-Indentation Tool/FormMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Auto_Indentation_Tool {
+    partial class FormMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.components = new System.ComponentModel.Container ();
+            this.listBoxFiles = new System.Windows.Forms.ListBox ();
+            this.contextMenuStripFilesList = new System.Windows.Forms.ContextMenuStrip (this.components);
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem ();
+            this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem ();
+            this.folderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem ();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem ();
+            this.buttonParse = new System.Windows.Forms.Button ();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog ();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog ();
+            this.contextMenuStripFilesList.SuspendLayout ();
+            this.SuspendLayout ();
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxFiles.ContextMenuStrip = this.contextMenuStripFilesList;
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point (0, 0);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size (334, 264);
+            this.listBoxFiles.TabIndex = 0;
+            this.listBoxFiles.KeyDown += new System.Windows.Forms.KeyEventHandler (this.listBoxFiles_KeyDown);
+            // 
+            // contextMenuStripFilesList
+            // 
+            this.contextMenuStripFilesList.Items.AddRange (new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.removeToolStripMenuItem});
+            this.contextMenuStripFilesList.Name = "contextMenuStripFilesList";
+            this.contextMenuStripFilesList.Size = new System.Drawing.Size (118, 48);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.DropDownItems.AddRange (new System.Windows.Forms.ToolStripItem[] {
+            this.filesToolStripMenuItem,
+            this.folderToolStripMenuItem});
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size (117, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            // 
+            // filesToolStripMenuItem
+            // 
+            this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size (107, 22);
+            this.filesToolStripMenuItem.Text = "Files";
+            this.filesToolStripMenuItem.Click += new System.EventHandler (this.filesToolStripMenuItem_Click);
+            // 
+            // folderToolStripMenuItem
+            // 
+            this.folderToolStripMenuItem.Name = "folderToolStripMenuItem";
+            this.folderToolStripMenuItem.Size = new System.Drawing.Size (107, 22);
+            this.folderToolStripMenuItem.Text = "Folder";
+            this.folderToolStripMenuItem.Click += new System.EventHandler (this.folderToolStripMenuItem_Click);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size (117, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            // 
+            // buttonParse
+            // 
+            this.buttonParse.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonParse.Location = new System.Drawing.Point (12, 276);
+            this.buttonParse.Name = "buttonParse";
+            this.buttonParse.Size = new System.Drawing.Size (310, 23);
+            this.buttonParse.TabIndex = 2;
+            this.buttonParse.Text = "Parse files";
+            this.buttonParse.UseVisualStyleBackColor = true;
+            this.buttonParse.Click += new System.EventHandler (this.buttonParse_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "All files|*.*";
+            this.openFileDialog.Multiselect = true;
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF (96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size (334, 311);
+            this.Controls.Add (this.buttonParse);
+            this.Controls.Add (this.listBoxFiles);
+            this.Name = "FormMain";
+            this.Text = "Auto-Indentation Tool";
+            this.contextMenuStripFilesList.ResumeLayout (false);
+            this.ResumeLayout (false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button buttonParse;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripFilesList;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem folderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ Tools/Auto-Indentation Tool/Auto-Indentation Tool/FormMain.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Auto_Indentation_Tool {
+    public partial class FormMain : Form {
+        public FormMain () {
+            InitializeComponent ();
+        }
+
+
+        private void buttonParse_Click (object sender, EventArgs e) {
+            Task ParseFiles = new Task (delegate {
+                foreach (string file in listBoxFiles.Items) {
+                    List<string> Input = new List<string> ();
+                    List<string> Output = new List<string> ();
+                    bool InActor = false;
+                    bool InStates = false;
+                    try {
+                        Input.AddRange (File.ReadAllLines (file));
+                    } catch {
+                        MessageBox.Show ("File is in use", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    } finally {
+
+                        foreach (string line in Input) {
+                            string parsedline = line;
+
+                            if (line.Contains ('}') && InStates)
+                                InStates = false;
+                            else if (line.Contains ('}') && InActor)
+                                InActor = false;
+
+                            // Trim spaces and tab characters from the start and end of the line
+                            parsedline = parsedline.Trim ();
+
+                            // Insert a space after commas
+                            parsedline = parsedline.Replace (", ", ",");
+                            parsedline = parsedline.Replace (",", ", ");
+
+                            // Insert a space before '('s
+                            parsedline = parsedline.Replace (" (", "(");
+                            parsedline = parsedline.Replace ("(", " (");
+
+                            // Add tab characters if we're parsing an actor
+                            if (InActor)
+                                parsedline = parsedline.Insert (0, "\t");
+                            if (InStates && !parsedline.EndsWith (":"))
+                                parsedline = parsedline.Insert (0, "\t");
+
+                            Debug.WriteLine (Regex.IsMatch (parsedline, "(\t\b:)").ToString ());
+
+                            // Add a newline character to the end of the line
+                            parsedline += '\n';
+
+                            Debug.WriteLine (parsedline);
+                            Output.Add (parsedline);
+
+                            if (line.Contains ('{') && !InActor)
+                                InActor = true;
+                            else if (line.Contains ('{') && !InStates && InActor)
+                                InStates = true;
+
+                            if (line.Contains ('}') && InStates)
+                                InStates = false;
+                            else if (line.Contains ('}') && InActor)
+                                InActor = false;
+                        }
+
+                        List<byte> OutputBytes = new List<byte> ();
+
+                        List<char> charList = new List<char> ();
+
+                        foreach (string line in Output)
+                            foreach (char character in line)
+                                charList.Add (character);
+
+                        foreach (char character in charList)
+                            OutputBytes.Add (Convert.ToByte (character));
+
+                        try {
+                            File.WriteAllBytes (file, OutputBytes.ToArray ());
+                        } catch {
+                            MessageBox.Show ("File is in use", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            });
+
+            ParseFiles.Start ();
+        }
+
+        private void filesToolStripMenuItem_Click (object sender, EventArgs e) {
+            if (openFileDialog.ShowDialog () == DialogResult.OK)
+                listBoxFiles.Items.AddRange (openFileDialog.FileNames);
+        }
+
+        private void folderToolStripMenuItem_Click (object sender, EventArgs e) {
+            if (folderBrowserDialog.ShowDialog () == DialogResult.OK) {
+                string[] Files;
+                string SelectedPath = folderBrowserDialog.SelectedPath;
+                string searchPattern = string.Empty;
+
+                if (Stuff.InputBox ("Input filename", "Input the name of files\n(Use \"*\" to add all files and \"*.<extension>\" to add all files with a certain extension)", ref searchPattern) == DialogResult.OK) {
+                    if (searchPattern.Equals (""))
+                        searchPattern = "*";
+
+                    Files = Directory.GetFiles (SelectedPath, searchPattern, SearchOption.AllDirectories);
+
+                    foreach (string file in Files) {
+                        listBoxFiles.Items.Add (file);
+                    }
+                }
+            }
+        }
+
+        private void listBoxFiles_KeyDown (object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Delete && listBoxFiles.SelectedIndex >= 0)
+                listBoxFiles.Items.RemoveAt (listBoxFiles.SelectedIndex);
+        }
+    }
+}
